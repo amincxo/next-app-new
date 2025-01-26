d90004ee4161c428f05a4ccfafe0d84958b99642
unknown <amincxo@gmail.com>
2025-01-26 07:46:11 +0330

    create daynamic route

---
+++ pages/users/[userId].js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function UserId() {
+  return (
+    <div>user unkown</div>
+  )
+}
+
+export default UserId
---
+++ pages/users/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Users() {
+  return (
+    <>
+        <div>Users List</div>
+        <h1>header</h1>
+        <p>user 1</p>
+        <p>user 2</p>
+    </>
+  )
+}
+
+export default Users
