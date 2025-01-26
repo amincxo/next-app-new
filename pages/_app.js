9a0fe5181fc6776b3b89c9c6ad0f8af7232ae0d1
unknown <amincxo@gmail.com>
2025-01-26 07:20:46 +0330

    app js global  set on application

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <p>header</p>
+    <Component {...pageProps} />
+    <p>footer</p>
+    </>
+    )
+
 }
 
 export default MyApp
