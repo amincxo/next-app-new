74dbeac97de1025e86b3cbb0416e270fc7084cd2
unknown <amincxo@gmail.com>
2025-02-01 01:21:25 +0330

    link in next

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,20 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <div className={styles.container}>
         <h1>aminborvayeh</h1>
+        <ul>
+            <li>
+                <Link href='/courses' replace >Go to courses</Link> 
+            </li>
+            <li>
+                <Link href='/users' >Go to courses</Link> 
+            </li>
+        </ul>
     </div>
   )
 }
