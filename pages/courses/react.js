93cf10831346fe57c9562bc2e89cde56d851cb50
unknown <amincxo@gmail.com>
2025-02-02 02:08:57 +0330

    styled jsx

--- pages/courses/react.js
+++ pages/courses/react.js
@@ -1,6 +1,20 @@
 function React() {
   return (
-    <h1>React</h1>
+    <>
+        <h1>React</h1>
+        <p>you should learn react before next.js you should learn react before next.js</p>
+        <h4>amin borvayeh</h4>
+        <style jsx>{`
+            h1 {
+                color : red;
+                background-color: silver;
+                
+            }
+            p {
+                width: 500px;
+            }
+        `}</style>
+    </>
   )
 }
 
